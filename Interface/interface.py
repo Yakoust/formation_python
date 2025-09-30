6c1aba658017598d7d8068b704cd8459b5803db4
Yakoust <30597077+Yakoust@users.noreply.github.com>
2025-09-30 14:16:23 +0200

    exo classes abstraites

---
+++ Interface/interface.py
@@ -0,0 +1,40 @@
+from abc import ABC
+
+class Interface(ABC):
+    __methods__ = ()
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        if cls is Interface:
+            return NotImplemented
+        return all(any(m in B.__dict__ for B in subclass.__mro__) for m in cls.__methods__)
+
+
+class Container(Interface):
+    __methods__ = ("__contains__",)
+
+class Sized(Interface):
+    __methods__ = ("__len__",)
+
+class SizedContainer(Container, Sized):
+    __methods__ = ("__len__", "__contains__")
+
+class Iterable(Interface):
+    __methods__ = ("__iter__",)
+
+class MaListe:
+    def __init__(self, data):
+        self.data = list(data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __contains__(self, item):
+        return item in self.data
+
+    def __iter__(self):
+        return iter(self.data)
+
+print(issubclass(MaListe, SizedContainer))
+print(issubclass(MaListe, Iterable))
+print(issubclass(MaListe, Container))
